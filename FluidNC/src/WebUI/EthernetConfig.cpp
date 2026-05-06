632a1f456bf22e47dfba63b8f13d457e5597b1e9
dpCreator.eu <105395725+val218@users.noreply.github.com>
2026-05-06 04:13:24 +0300

    Update EthernetConfig.cpp

--- FluidNC/src/WebUI/EthernetConfig.cpp
+++ FluidNC/src/WebUI/EthernetConfig.cpp
@@ -3,6 +3,7 @@
 #ifdef ENABLE_ETHERNET
 
 #include <ETH.h>
+#include <SPI.h>
 #include <esp_log.h>
 
 static const char* TAG = "Ethernet";
@@ -10,15 +11,15 @@ static const char* TAG = "Ethernet";
 // dpCREATOR R2 - confirmed from schematic
 #define W5500_CS_GPIO    14
 #define W5500_INT_GPIO   9
-#define W5500_RST_GPIO   -1   // no RST pin on this board
+#define W5500_RST_GPIO   -1
 #define W5500_PHY_ADDR   1
-#define W5500_SPI_HOST   SPI2_HOST
+#define W5500_SPI_HOST   FSPI_HOST   // SPI2 on S3 = FSPI_HOST in older framework
 #define W5500_SPI_SCK    12
 #define W5500_SPI_MISO   13
 #define W5500_SPI_MOSI   11
 #define W5500_SPI_MHZ    20
 
-static void eth_event_handler(arduino_event_id_t event) {
+static void eth_event_handler(WiFiEvent_t event) {
     switch (event) {
         case ARDUINO_EVENT_ETH_START:
             ESP_LOGI(TAG, "Started");
@@ -44,19 +45,17 @@ static void eth_event_handler(arduino_event_id_t event) {
 void ethernet_init() {
     ESP_LOGI(TAG, "Init W5500 CS=IO%d INT=IO%d", W5500_CS_GPIO, W5500_INT_GPIO);
 
-    Network.onEvent(eth_event_handler);
+    WiFi.onEvent(eth_event_handler);
 
     ETH.begin(
-        ETH_PHY_W5500,
-        W5500_PHY_ADDR,
-        W5500_CS_GPIO,
-        W5500_INT_GPIO,
-        W5500_RST_GPIO,
-        W5500_SPI_HOST,
-        W5500_SPI_SCK,
-        W5500_SPI_MISO,
-        W5500_SPI_MOSI,
-        W5500_SPI_MHZ
+        W5500_PHY_ADDR,      // phy_addr
+        W5500_INT_GPIO,      // intr_pin
+        W5500_RST_GPIO,      // rst_pin
+        W5500_CS_GPIO,       // cs_pin
+        W5500_SPI_SCK,       // sck_pin
+        W5500_SPI_MISO,      // miso_pin
+        W5500_SPI_MOSI,      // mosi_pin
+        W5500_SPI_HOST       // spi_host
     );
 
     ESP_LOGI(TAG, "W5500 init complete");
