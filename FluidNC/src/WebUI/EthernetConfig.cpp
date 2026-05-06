d59fa81e5d1d336b5dcadc0ff30eea538b3078c6
dpCreator.eu <105395725+val218@users.noreply.github.com>
2026-05-06 04:08:29 +0300

    Update EthernetConfig.cpp

--- FluidNC/src/WebUI/EthernetConfig.cpp
+++ FluidNC/src/WebUI/EthernetConfig.cpp
@@ -2,36 +2,38 @@
 
 #ifdef ENABLE_ETHERNET
 
-#include <esp_eth.h>
-#include <esp_eth_mac.h>
-#include <esp_eth_phy.h>
-#include <esp_netif.h>
-#include <esp_event.h>
-#include <driver/spi_master.h>
+#include <ETH.h>
 #include <esp_log.h>
 
-// dpCREATOR R2 - confirmed from schematic
-#define W5500_CS_GPIO   14
-#define W5500_INT_GPIO  9
-#define W5500_SPI_HOST  SPI2_HOST
-#define W5500_SPI_HZ    20000000
-
 static const char* TAG = "Ethernet";
-static esp_eth_handle_t s_eth_handle = NULL;
 
-static void eth_event_handler(void* arg, esp_event_base_t event_base,
-                               int32_t event_id, void* event_data) {
-    switch (event_id) {
-        case ETHERNET_EVENT_CONNECTED:
+// dpCREATOR R2 - confirmed from schematic
+#define W5500_CS_GPIO    14
+#define W5500_INT_GPIO   9
+#define W5500_RST_GPIO   -1   // no RST pin on this board
+#define W5500_PHY_ADDR   1
+#define W5500_SPI_HOST   SPI2_HOST
+#define W5500_SPI_SCK    12
+#define W5500_SPI_MISO   13
+#define W5500_SPI_MOSI   11
+#define W5500_SPI_MHZ    20
+
+static void eth_event_handler(arduino_event_id_t event) {
+    switch (event) {
+        case ARDUINO_EVENT_ETH_START:
+            ESP_LOGI(TAG, "Started");
+            ETH.setHostname("fluidnc");
+            break;
+        case ARDUINO_EVENT_ETH_CONNECTED:
             ESP_LOGI(TAG, "Link Up");
             break;
-        case ETHERNET_EVENT_DISCONNECTED:
+        case ARDUINO_EVENT_ETH_GOT_IP:
+            ESP_LOGI(TAG, "IP: %s", ETH.localIP().toString().c_str());
+            break;
+        case ARDUINO_EVENT_ETH_DISCONNECTED:
             ESP_LOGI(TAG, "Link Down");
             break;
-        case ETHERNET_EVENT_START:
-            ESP_LOGI(TAG, "Started");
-            break;
-        case ETHERNET_EVENT_STOP:
+        case ARDUINO_EVENT_ETH_STOP:
             ESP_LOGI(TAG, "Stopped");
             break;
         default:
@@ -39,62 +41,24 @@ static void eth_event_handler(void* arg, esp_event_base_t event_base,
     }
 }
 
-static void got_ip_handler(void* arg, esp_event_base_t event_base,
-                            int32_t event_id, void* event_data) {
-    ip_event_got_ip_t* event = (ip_event_got_ip_t*)event_data;
-    ESP_LOGI(TAG, "IP: " IPSTR, IP2STR(&event->ip_info.ip));
-    ESP_LOGI(TAG, "GW: " IPSTR, IP2STR(&event->ip_info.gw));
-}
-
 void ethernet_init() {
-    ESP_LOGI(TAG, "Init W5500 CS=IO14 INT=IO9");
+    ESP_LOGI(TAG, "Init W5500 CS=IO%d INT=IO%d", W5500_CS_GPIO, W5500_INT_GPIO);
 
-    // Do NOT call spi_bus_initialize() here.
-    // FluidNC SD card driver already owns SPI2_HOST.
-    // W5500 is added as a second device on the existing bus.
+    Network.onEvent(eth_event_handler);
 
-    spi_device_interface_config_t spi_devcfg = {};
-    spi_devcfg.mode           = 0;
-    spi_devcfg.clock_speed_hz = W5500_SPI_HZ;
-    spi_devcfg.queue_size     = 20;
-    spi_devcfg.spics_io_num   = W5500_CS_GPIO;
+    ETH.begin(
+        ETH_PHY_W5500,
+        W5500_PHY_ADDR,
+        W5500_CS_GPIO,
+        W5500_INT_GPIO,
+        W5500_RST_GPIO,
+        W5500_SPI_HOST,
+        W5500_SPI_SCK,
+        W5500_SPI_MISO,
+        W5500_SPI_MOSI,
+        W5500_SPI_MHZ
+    );
 
-    eth_w5500_config_t w5500_config = ETH_W5500_DEFAULT_CONFIG(W5500_SPI_HOST, &spi_devcfg);
-    w5500_config.int_gpio_num = W5500_INT_GPIO;
-
-    eth_mac_config_t mac_config   = ETH_MAC_DEFAULT_CONFIG();
-    mac_config.rx_task_stack_size = 4096;
-
-    esp_eth_mac_t* mac = esp_eth_mac_new_w5500(&w5500_config, &mac_config);
-    if (!mac) {
-        ESP_LOGE(TAG, "W5500 MAC create failed");
-        return;
-    }
-
-    eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
-    phy_config.reset_gpio_num   = -1;
-
-    esp_eth_phy_t* phy = esp_eth_phy_new_w5500(&phy_config);
-    if (!phy) {
-        ESP_LOGE(TAG, "W5500 PHY create failed");
-        return;
-    }
-
-    esp_eth_config_t eth_config = ETH_DEFAULT_CONFIG(mac, phy);
-    esp_err_t err = esp_eth_driver_install(&eth_config, &s_eth_handle);
-    if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Driver install failed: %s", esp_err_to_name(err));
-        return;
-    }
-
-    esp_netif_config_t netif_cfg = ESP_NETIF_DEFAULT_ETH();
-    esp_netif_t* eth_netif       = esp_netif_new(&netif_cfg);
-    esp_netif_attach(eth_netif, esp_eth_new_netif_glue(s_eth_handle));
-
-    esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL);
-    esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_handler, NULL);
-
-    esp_eth_start(s_eth_handle);
     ESP_LOGI(TAG, "W5500 init complete");
 }
 
